ff721c86a20579fd0e473b9e28afc5fb264a1301
Arvin <argu.lim.up@phinmaed.com>
2023-12-08 11:23:20 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jerome-block6-Quiz"
+include(":app")
+ 
